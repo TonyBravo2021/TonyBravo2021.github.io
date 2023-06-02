3f7326bea49b7944d2e0570d50f434cb83125bf0
TonyBC <84157713+TonyBravo2021@users.noreply.github.com>
2023-06-02 16:43:22 -0500

    Update guardar_datos.php

--- guardar_datos.php
+++ guardar_datos.php
@@ -1,12 +1,19 @@
 <?php
-$datos = $_POST['datos'];
+// Obtener los datos enviados mediante la solicitud POST
+$data = json_decode(file_get_contents('php://input'), true);
 
-$archivo = fopen("/datos.txt", "a");
-if ($archivo) {
-  fwrite($archivo, $datos . "\n");
-  fclose($archivo);
-  http_response_code(200);
-} else {
-  http_response_code(500);
-}
+// Obtener los valores de los campos del formulario
+$name = $data['name'];
+$email = $data['email'];
+$message = $data['message'];
+
+// Aquí puedes realizar la lógica de guardado de datos en el formato que desees (por ejemplo, base de datos, archivo, etc.)
+
+// Ejemplo de guardado en un archivo de texto
+$file = 'datos.txt';
+$content = "Nombre: $name\nEmail: $email\nMensaje: $message\n\n";
+file_put_contents($file, $content, FILE_APPEND);
+
+// Devolver una respuesta al cliente indicando el estado del guardado
+http_response_code(200);
 ?>
